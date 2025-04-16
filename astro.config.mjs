27e1510b15fea987817e770c060206aa3304479a
louis17782 <luissanteliz22@gmail.com>
2025-04-16 16:40:14 -0400

    add adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,26 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://venezolana-de-acero.netlify.app/",
+  prefetch: true,
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler'
+        }
+      }
+    },
+    server: {
+      hmr: true,
+    }
+  },
+  image: {
+    // Used for all `<Image />` and `<Picture />` components unless overridden
+    experimentalLayout: 'responsive',
+  },
+  experimental: {
+    responsiveImages: true,
+  },
+});
