5f76e0c8d206dd5586b48888da5297f93f7cc462
louis17782 <luissanteliz22@gmail.com>
2025-04-16 16:49:21 -0400

    changes url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://venezolana-de-acero.netlify.app/",
+  site: "https://venezolanadeacero.netlify.app/",
   prefetch: true,
   vite: {
     css: {
